437940ea2ddd32847fd9f05b06b5f8b33e6a1151
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-02-23 18:35:30 +0100

    inicio db

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server")
+
+// crea el objeto de TODO mi servidor
+const server = jsonServer.create() 
+
+// hacer las configuraciones basicas de mi servidor
+const middlewares = jsonServer.defaults() 
+server.use(middlewares)
+
+// permitir acceso de cualquier cliente a mi servidor
+const allowAccessFromAnywhere = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+}
+server.use(allowAccessFromAnywhere)
+
+// crea TODAS las rutas de server a la base de datos indicada.
+const router = jsonServer.router("db.json") 
+server.use(router)
+
+const PORT = 5001
+
+server.listen(PORT, () => {
+  console.log(`Servidor JSON andando! en el puerto ${PORT}`)
+  console.log(`Acceso Local en: http://localhost:${PORT}`)
+})
