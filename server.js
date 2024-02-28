ffc5be2597110baa7728cddc6adda37db34be5a6
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-02-28 10:53:51 +0100

    configuracion para deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.use(allowAccessFromAnywhere)
 const router = jsonServer.router("db.json") 
 server.use(router)
 
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 
 server.listen(PORT, () => {
   console.log(`Servidor JSON andando! en el puerto ${PORT}`)
